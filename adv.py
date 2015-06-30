3bf7e26b1bfa6edff2d5282f26d8785701e1f2ac
Denys Aleksandrov <dnaleksandrov@gmail.com>
2015-06-30 17:29:10 +0200

    incremental processing of XML docs

--- adv.py
+++ adv.py
@@ -6,18 +6,15 @@ Parses suplied output of the command
 to the exabgp neighbor as a valid bgp update
 
 TODO: 
-    - write XML Incremental parser as bgp full view is a huge xml file
     - add atomic, aggregator, med, local preference attributes
 """
-#from sys import argv
+import re
 from sys import stdout
 from time import sleep
 from lxml import etree
 from update import Update
 from bintrees import RBTree
-#from pprint import pprint as pp
-
-#_, FILE = argv
+from xml.etree.ElementTree import iterparse
 
 def send_update(tree):
     '''
@@ -81,20 +78,45 @@ class XMLNamespaces:
 
     def __call__(self, path):
         return path.format_map(self._namespaces)
+def parse_and_remove(file, path):
+    '''
+    allows incremental processing of XML documents
+    '''
+    path_parts = path.split('/')
+    context = iterparse(file, ('start', 'end'))
+    next(context)
+    tag_stack = []
+    elem_stack = []
+    for event, elem in context:
+        if event == 'start':
+            tag = re.sub('{[^}]*}', '', elem.tag)
+            tag_stack.append(tag)
+            elem_stack.append(elem)
+        elif event == 'end':
+            if tag_stack == path_parts:
+                yield elem
+                elem_stack[-2].remove(elem)
+            try:
+                tag_stack.pop()
+                elem_stack.pop()
+            except IndexError:
+                pass
 
 def main():
     '''
     main
     '''
-    #input_xml = etree.parse('/home/den/small_table_xml')
-    input_xml = etree.parse('/home/den/full2')
-    root = input_xml.getroot()
+    #input_xml = etree.parse('/home/den/full_table_xml')
+    input_xml = parse_and_remove('/home/den/full_table_xml',
+                                 'route-information/route-table/rt')
+    #root = input_xml.getroot()
     prep = 'http://xml.juniper.net/junos/12.3R3/junos-routing'
     ns = XMLNamespaces(prep=prep)
     tree = RBTree()
-    for i in root.findall(ns('{prep}route-information/ \
-                             {prep}route-table/        \
-                             {prep}rt')):
+    #for i in root.findall(ns('{prep}route-information/ \
+    #                         {prep}route-table/        \
+    #                         {prep}rt')):
+    for i in input_xml:
         prefix = i.find(ns('{prep}rt-destination')).text
         mask = i.find(ns('{prep}rt-prefix-length')).text
         key = (ip_to_int(prefix), mask)
@@ -103,9 +125,7 @@ def main():
         asp = i.find(ns('{prep}rt-entry/{prep}as-path')).text.rstrip()
         asp = get_as_path_origin_atomic(asp)
         community = []
-        for comm in i.findall(ns('{prep}rt-entry/     \
-                                 {prep}communities/   \
-                                 {prep}community')):
+        for comm in i.findall(ns('{prep}rt-entry/{prep}communities/{prep}community')):
             community.append(comm.text)
         atomic_bool = False
         if len(asp) == 2:
@@ -127,42 +147,6 @@ def main():
                             community=' '.join(community))
         tree.insert(key, update)
     send_update(tree)
-    #o = xmltodict.parse(etree.tostring(input_xml, encoding='utf-8'))
-    #j = json.loads(json.dumps(o))
-    #tree = RBTree()
-    #for route in o['rpc-reply']['route-information']['route-table'][0]['rt']:
-    #    prefix = route['rt-destination']
-    #    mask = route['rt-prefix-length']
-    #    key = (ip_to_int(prefix), mask)
-    #    next_hop = route['rt-entry']['nh']['to']
-    #    if 'communities' in route['rt-entry']:
-    #        community = route['rt-entry']['communities']['community']
-    #        if not hasattr(community, 'lower'):
-    #            community = ' '.join(community)
-    #    else:
-    #        community = ''
-    #    asp = get_as_path_origin_atomic(route['rt-entry']['as-path'])
-    #    atomic_bool = False
-    #    if len(asp) == 2:
-    #        as_path = asp[0]
-    #        origin = asp[1]
-    #    else:
-    #        atomic_bool = True
-    #        as_path = asp[0]
-    #        origin = asp[1]
-    #        atomic = asp[2]
-    #        aggregator = asp[3]
-    #    if atomic_bool:
-    #        update = Update(prefix, mask, nh=next_hop, as_path=as_path,
-    #                        origin=origin,
-    #                        community=community)
-    #    else:
-    #        update = Update(prefix, mask, nh=next_hop, as_path=as_path,
-    #                        origin=origin,
-    #                        community=community)
-    #    tree.insert(key, update)
-
-
 
 if __name__ == '__main__':
     main()
